0bb5001c1e85ee52af3e436021a5babc230ff4d0
Justyvevo <dedpul3003@gmail.com>
2023-06-15 14:50:13 +0300

    super commit

--- balls.py
+++ balls.py
@@ -1,53 +1,180 @@
-import os
 import requests
-import telebot
+from env import TOKEN
+from enw import API_KEY
+import time
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+import random
+import logging
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+import schedule
+from pprint import pprint
+from schema import Root
+import requests
+import asyncio
+# Загружаем переменные окружения
+load_dotenv()
 
-# Define the bot token and weather API key as environment variables
-bot_token = os.environ.get('BOT_TOKEN')
-weather_api_key = os.environ.get('WEATHER_API_KEY')
+# Initialize bot and dispatcher
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-# Create a bot instance using the bot token
-bot = telebot.TeleBot(bot_token)
+# Время кэширования данных о погоде (в секундах)
+CACHE_TIME = 300
 
-# Define a message handler for the /start command
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Welcome to the Weather Bot! Please enter the name of a city to get the current weather and clothing recommendations.")
+# Словарь для кэширования данных о погоде
+weather_cache = {}
 
-# Define a message handler for text messages
-@bot.message_handler(func=lambda message: True)
-def send_weather(message):
-    # Get the city name from the user's message
-    city_name = message.text
+# Клавиатура с кнопкой для включения отправки уведомлений
 
-    # Construct the API URL for the weather API
-    api_url = f'https://api.weatherapi.com/v1/current.json?key={weather_api_key}&q={city_name}'
 
-    # Send a request to the weather API
+
+
+
+
+
+# Обработчик команды /start
+start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Включить уведомления о погоде'))
+
+# Флаг, определяющий, включены ли уведомления
+notifications_enabled = False
+
+# Обработчик команды /start
+@dp.message_handler(commands=['start', 'help'], state='*')
+async def send_welcome(message: types.Message, state: FSMContext):
+    await message.reply("Привет! Я бот, который может сообщить тебе погоду в любом городе. Просто напиши мне название города.")
+    # Проверяем состояние флага уведомлений и устанавливаем соответствующую клавиатуру
+    if notifications_enabled:
+        keyboard = start_keyboard.remove('Включить уведомления о погоде')
+    else:
+        keyboard = start_keyboard
+    await message.reply("Чтобы включить уведомления о погоде, пропиши команду /set_city.", reply_markup=keyboard)
+
+# Запускаем планировщик задач в отдельном потоке
+
+# Обработчик нажатия кнопки
+@dp.message_handler(Text(equals='Включить уведомления о погоде'), state="*")
+async def enable_notifications(message: types.Message, state: FSMContext):
+    global notifications_enabled
+    # await state.update_data(city="Moscow")
+    data = await state.get_data()
+    city = data.get("city")
+    if not city: 
+        ... # Введите ваш город
+        await state.set_state("ask_city")
+    else:
+        if not notifications_enabled:
+            notifications_enabled = True
+            # Здесь вызывайте функцию send_weather_notification с выбранным городом
+            # Не забудьте добавить асинхронность к функции
+            # Пример: await send_weather_notification(city_name)
+            await message.reply("Уведомления о погоде включены.")
+        else:
+            await message.reply("Уведомления о погоде уже включены.")
+
+# callback | CallbackData
+
+@dp.message_handler(commands=["set_city"], state = "*")
+async def _(message: types.Message, state: FSMContext):
+    await message.reply("Введите название города")
+    await state.set_state("ask_city")
+
+dp.message_handler(commands=['delete_set'], state='*')
+async def send_welcome(message: types.Message, state: FSMContext):
+    await message.reply("Уведомления были выкллючены")
+  
+
+
+# Обработчик сообщений с текстом
+@dp.message_handler(state="ask_city")
+async def _(message: types.Message, state: FSMContext):
+    city = message.text
+    print(city)
+    await state.update_data({"city": city})
+    ... 
+    await state.reset_state(with_data=False) # ...
+    weather = get_weather(city_name=city) 
+    await message.reply(weather)
+    await asyncio.sleep(1)
+    await message.reply(weather)
+
+
+
+# Обработчик сообщений с текстом
+def get_weather(city_name: str): # Заменить на функцию, которая ничего не отправляет пользователю; А просто возвращает строку или что-то в этом роде. 
+    # Формирование URL API для API погоды
+    api_url = f'https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city_name}&lang=ru'
+    api_url = f'https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={city_name}&lang=ru'
+
+    # Отправка запроса к API погоды
     response = requests.get(api_url)
+        
 
-    # Check if the request was successful
+    # Проверка успешности запроса
     if response.status_code == 200:
-        # Parse the JSON response
+        # Разбор JSON-ответа
         data = response.json()
-
-        # Get the temperature and weather description from the response
+        root = Root.from_dict(data)
+        pprint(data)
+        # Получение температуры и описания погоды из ответа
         temperature = data['current']['temp_c']
         weather_description = data['current']['condition']['text']
+        temperaturefeel = data['current']['feelslike_c']
+        windspdfalse = data['current']['wind_kph']
+        winddir =data['current']['wind_dir']
+        windspdtrue = windspdfalse * 5/18
+        localtm = data['location']['localtime']
+        humidityy =data['current']['humidity']
+        pressure =data['current']['pressure_mb']
 
-        # Determine the appropriate clothing recommendation based on the temperature
+        sunrisee = []
+        sunsett = []
+        rainchance = []
+
+        for estimation in data['forecast']['forecastday']:
+            sunrisee.append(estimation['astro']['sunrise'])
+            sunsett.append(estimation['astro']['sunset'])
+            rainchance.append(estimation['day']['daily_chance_of_rain'])
+
+
+        # Определение соответствующей рекомендации по одежде на основе температуры
         if temperature < 10:
-            clothing_recommendation = 'It is very cold outside. You should wear a heavy coat, gloves, and a hat.'
+            clothing_recommendation = 'Сейчас очень холодно, поэтому Вы должны надеть теплую куртку, перчатки и шапку.'
         elif temperature < 20:
-            clothing_recommendation = 'It is a bit chilly outside. You should wear a light jacket or sweater.'
+            clothing_recommendation = 'Сейчас прохладно, поэтому Вы можете надеть легкую куртку или свитер.'
         else:
-            clothing_recommendation = 'It is warm outside. You can wear a t-shirt and shorts.'
+            clothing_recommendation = 'Сейчас тепло, поэтому Вы можете надеть футболку и шорты.'
 
-        # Send a message to the user with the temperature, weather description, and clothing recommendation
-        bot.reply_to(message, f'The temperature in {city_name} is {temperature}°C and the weather is {weather_description}. {clothing_recommendation}')
+        # Отправка сообщения пользователю с температурой, описанием погоды и рекомендацией по одежде
+        return (f"Местное время: {localtm}\n"
+            f"Погода в городе: {city_name}\nТемпература: {temperature}C°\n"
+            f"Влажность: {humidityy}%\nДавление: {pressure} мм.рт.ст\nВетер: {windspdtrue:.2f} м/с\nНапраление ветра: {winddir}\n"
+            f"Восход солнца {sunrisee}: \nЗакат солнца {sunsett}: \nВероятность осадков: {rainchance}\n"
+            f"Хорошего дня!"
+            )
     else:
-        # Send an error message to the user if the request was unsuccessful
-        bot.reply_to(message, 'Sorry, I could not get the weather information for that city. Please try again later.')
+        # Отправка сообщения об ошибке пользователю, если запрос был неудачным
+        return('Извините, я не смог получить информацию о погоде для этого города. Пожалуйста, попробуйте еще раз позже.')
 
-# Start the bot
-bot.polling()
+
+# MUST BE AFTER ALL COMMANDS
+@dp.message_handler(Text(startswith="/"))
+async def _(message: types.Message, state: FSMContext):
+     await message.reply("Такой команды не существует, попробуй снова")
+
+# Запускаем бота
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+    @dp.message_handler(Text(equals='Что такое dn?'))
+async def enable_notifications(message: types.Message, state: FSMContext):
+    await message.reply("Deez nuts in your mouth!")
+    await bot.send_video(message.chat.id, 'https://media.tenor.com/_8YhYtl4gWAAAAAC/deez-nuts.gif', None, 'Text')
+
+@dp.message_handler(Text(equals='Устрой дождь из лягушек'))
+async def enable_notifications(message: types.Message, state: FSMContext):
+   await bot.send_video(message.chat.id, 'https://i.gifer.com/MI2d.gif', None, 'Text')
